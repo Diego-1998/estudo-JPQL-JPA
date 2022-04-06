dad61fda4e741978b8149e67931ad7c9ff69c82b
Diego leandro <diegolgaspar000@gmail.com>
2022-04-06 17:13:37 -0300

    refatorando classe usuario

--- src/main/java/com/jpql/sistemausuarios/ConsultasComJPQL.java
+++ src/main/java/com/jpql/sistemausuarios/ConsultasComJPQL.java
@@ -1,4 +1,31 @@
 package com.jpql.sistemausuarios;
 
+import com.jpql.sistemausuarios.model.Usuario;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
 public class ConsultasComJPQL {
+
+    public static void main(String[] args) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("Usuarios-PU");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+
+        primeiraConsulta(entityManager);
+
+        entityManager.close();
+        entityManagerFactory.close();
+    }
+
+        public static void  primeiraConsulta(EntityManager entityManager){
+
+            String jpql = "select u from Usuario u";
+            TypedQuery<Usuario> typedQuery = entityManager.createQuery(jpql, Usuario.class);
+            List<Usuario> lista = typedQuery.getResultList();
+            lista.forEach( u -> System.out.println(u.getId() + ", " + u.getNome()));
+    }
 }
